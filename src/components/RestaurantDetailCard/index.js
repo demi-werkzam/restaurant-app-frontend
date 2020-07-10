a703ce8a8c7b607e1ca8a56e516de192cdc0f23b
demi-werkzam <d.werkzam@gmail.com>
2020-07-10 17:24:37 +0200

    Basic styling added

--- src/components/RestaurantDetailCard/index.js
+++ src/components/RestaurantDetailCard/index.js
@@ -1,26 +1,44 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
+import { Jumbotron } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Accordion from "react-bootstrap/Accordion";
 
 export default function RestaurantCard(props) {
   const { id, name, address, email } = props.data;
 
   return (
     <Container>
+      <Jumbotron>
+        <h1>{`${name}`}</h1>
+      </Jumbotron>
       <Row>
         <Col>
-          <Card>
-            <Card.Body>
-              <Card.Title>{`Name ${name}`}</Card.Title>
-              <Card.Title>{`Address ${address}`}</Card.Title>
-              <Card.Title>{`Telephone Number: Has to be added`}</Card.Title>
-              <Card.Title>{`Email ${email}`}</Card.Title>
-            </Card.Body>
+          <Card border="light" bg="dark">
+            <Accordion className="text-right">
+              <Card.Header>
+                <Accordion.Toggle
+                  bg="Light"
+                  as={Button}
+                  variant="light"
+                  eventKey="0"
+                >
+                  More information
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <Card.Text className="text-right">{`Address: ${address}`}</Card.Text>
+                  <Card.Text className="text-right">{`Telephone Number: Has to be added`}</Card.Text>
+                  <Card.Text className="text-right">{`Email: ${email}`}</Card.Text>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Accordion>
           </Card>
         </Col>
       </Row>
