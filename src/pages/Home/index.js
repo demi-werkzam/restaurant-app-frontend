f36a418853b70a458ab4228d62633deb6638c506
demi-werkzam <d.werkzam@gmail.com>
2020-07-13 13:02:35 +0200

    minor fix on homepage with map

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory, Link } from "react-router-dom";
+
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+
+import "./index.css";
 
 import { Jumbotron } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
@@ -11,6 +17,7 @@ import RestaurantCard from "../../components/RestaurantCard";
 
 export default function Home() {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     dispatch(fetchRestaurants);
@@ -18,20 +25,29 @@ export default function Home() {
 
   const restaurants = useSelector(selectRestaurants) || [];
 
+  const goToRestaurant = (id) => {
+    history.push(`/restaurants/${id}`);
+  };
+
   return (
     <Container>
       <Jumbotron>
-        <h1>List of restaurants</h1>
+        <h1>restaurants</h1>
       </Jumbotron>
-      {restaurants &&
-        restaurants.map((restaurant, i) => {
-          return (
-            <p key={i}>
-              {" "}
-              <RestaurantCard data={restaurant} />
-            </p>
-          );
-        })}
+      <Map center={[52.370216, 4.895168]} zoom={12}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {restaurants &&
+          restaurants.map((restaurant) => (
+            <Marker
+              key={restaurant.id}
+              position={[restaurant.latitude, restaurant.longitude]}
+              onClick={() => goToRestaurant(restaurant.id)}
+            />
+          ))}
+      </Map>
     </Container>
   );
 }
