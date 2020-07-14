32d8d4a8d568d7d72ce0433358d784760d0fe062
demi-werkzam <d.werkzam@gmail.com>
2020-07-14 22:46:21 +0200

    styling to the homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,17 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
+import { Map, Marker, TileLayer } from "react-leaflet";
 
 import "./index.css";
 
 import { Jumbotron } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import Carousel from "react-bootstrap/Carousel";
 
 import { selectRestaurants } from "../../store/restaurants/selectors";
 import { fetchRestaurants } from "../../store/restaurants/actions";
-
-import RestaurantCard from "../../components/RestaurantCard";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -31,9 +29,18 @@ export default function Home() {
 
   return (
     <Container>
-      <Jumbotron>
-        <h1>restaurants</h1>
-      </Jumbotron>
+      <Carousel className="slide-show">
+        <Carousel.Item>
+          <img alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img alt="Second slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
+
       <Map center={[52.370216, 4.895168]} zoom={12}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
