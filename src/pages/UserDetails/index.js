4af8e910e7754796e726c106c349ac50a11e0104
demi-werkzam <d.werkzam@gmail.com>
2020-07-21 11:46:58 +0200

    Added bootstrap navbar to the page

--- src/pages/UserDetails/index.js
+++ src/pages/UserDetails/index.js
@@ -1,5 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Nav } from "react-bootstrap";
 
 export default function UserDetails() {
-  return <div>User Details</div>;
+  return (
+    <Nav fill variant="tabs">
+      <Nav.Item>
+        <Nav.Link href="/home"> Visited </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2"> Liked </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-3"> Requested</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
 }
